273fd58a4b931f1d089bb71a3cf7dfe5a959a310
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2025-12-04 18:03:35 -0300

    require em novos arquivos JS para o footer

--- src/app/design/frontend/Avanti/innovation/Magento_Theme/requirejs-config.js
+++ src/app/design/frontend/Avanti/innovation/Magento_Theme/requirejs-config.js
@@ -1,5 +1,7 @@
 var config = {
     deps: [
-        "Magento_Theme/js/move-minicart"
+        "Magento_Theme/js/move-minicart",
+        "Magento_Theme/js/footer-accordion",
+        "Magento_Theme/js/format-right-text"
     ]
 };
