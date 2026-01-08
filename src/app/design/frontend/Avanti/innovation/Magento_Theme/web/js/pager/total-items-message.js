e53367f48ae82c5569a33996ef67d03c5fda2e05
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-01-08 20:56:11 -0300

    uso de variáveis

--- src/app/design/frontend/Avanti/innovation/Magento_Theme/web/js/pager/total-items-message.js
+++ src/app/design/frontend/Avanti/innovation/Magento_Theme/web/js/pager/total-items-message.js
@@ -6,12 +6,14 @@ require([
         media: '(max-width: 767px)',
         entry: function () {
             // Armazenar o texto original para restaurar depois
-            if (!$('#toolbar-amount').data('original-text')) { 
-                $('#toolbar-amount').data('original-text', $('#toolbar-amount').text());
+            var $toolBar = $('#toolbar-amount');
+            
+            if (!$toolBar.data('original-text')) { 
+                $toolBar.data('original-text', $toolBar.text());
             }          
             // Substituir o texto de paginação pelo total de itens encontrados
             var totalItemsText = $('span[data-total-items]').data('total-items');
-            $('#toolbar-amount').text(totalItemsText);
+            $toolBar.text(totalItemsText);
         },
         exit: function () {
             // Restaurar o texto original ao sair da mídia query
