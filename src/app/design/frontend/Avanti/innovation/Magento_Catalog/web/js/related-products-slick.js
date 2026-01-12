8762b5e064080163d62d4eb1ac050534aa4d2f87
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-01-12 18:05:11 -0300

    carrossél com slick para os produtos relacionados

---
+++ src/app/design/frontend/Avanti/innovation/Magento_Catalog/web/js/related-products-slick.js
@@ -0,0 +1,39 @@
+require([
+    'jquery',
+    'slick'
+], function ($) {
+
+    var $carousel = $('.products.list.items.product-items');
+
+    if ($carousel.length && !$carousel.hasClass('slick-initialized')) {
+        $carousel.slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            dots: true,
+            arrows: true,
+            infinite: false,
+            responsive: [
+                {
+                    breakpoint: 1180,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                        infinite: false,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        infinite: false,
+                        dots: true
+                    }
+                }
+            ]
+        });
+    }
+});
+
+
