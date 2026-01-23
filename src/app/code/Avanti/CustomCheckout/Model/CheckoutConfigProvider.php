077db6504e07e66719d4437cef3983908b7eee11
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-01-23 17:59:05 -0300

    Comentários

--- src/app/code/Avanti/CustomCheckout/Model/CheckoutConfigProvider.php
+++ src/app/code/Avanti/CustomCheckout/Model/CheckoutConfigProvider.php
@@ -1,32 +1,78 @@
-<?php 
+<?php
 
-declare(strict_types=1); 
+declare(strict_types=1);
 
-namespace Avanti\CustomCheckout\Model; 
-use Magento\Checkout\Model\ConfigProviderInterface; 
-use Magento\Cms\Block\BlockFactory; 
-use Magento\Store\Model\StoreManagerInterface; 
- 
-class CheckoutConfigProvider implements ConfigProviderInterface 
-{ 
-    public function __construct( 
-        private BlockFactory $cmsBlockFactory, 
-        private StoreManagerInterface $storeManager 
-    ) {} 
- 
-    public function getConfig(): array 
-    { 
-        $storeId = (int) $this->storeManager->getStore()->getId(); 
- 
-        /** @var \Magento\Cms\Block\Block $block */ 
-        $block = $this->cmsBlockFactory->create(); 
-        $block->setData('block_id', 'fulfillment_status'); // identifier do CMS Block 
-        $block->setStoreId($storeId); 
- 
-        return [ 
-            'avanti_customcheckout' => [ 
-                'fulfillment_status_html' => $block->toHtml() 
-            ] 
-        ]; 
-    } 
-} 
+namespace Avanti\CustomCheckout\Model;
+
+// Interface que todo ConfigProvider do checkout deve implementar
+use Magento\Checkout\Model\ConfigProviderInterface;
+
+// Factory para criar instâncias de blocos CMS dinamicamente
+use Magento\Cms\Block\BlockFactory;
+
+// Interface para obter informações da loja atual (store, website, etc)
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Classe responsável por fornecer configurações personalizadas
+ * para o frontend do checkout via window.checkoutConfig
+ */
+class CheckoutConfigProvider implements ConfigProviderInterface
+{
+    /**
+     * Construtor com injeção de dependências via DI do Magento
+     *
+     * @param BlockFactory $cmsBlockFactory
+     *        Usado para criar um bloco CMS programaticamente
+     *
+     * @param StoreManagerInterface $storeManager
+     *        Usado para descobrir qual é a store atual
+     */
+    public function __construct(
+        private BlockFactory $cmsBlockFactory,
+        private StoreManagerInterface $storeManager
+    ) {}
+
+    /**
+     * Método obrigatório da interface ConfigProviderInterface
+     *
+     * Tudo que for retornado aqui será mesclado em:
+     * window.checkoutConfig no frontend (JavaScript)
+     *
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        // Obtém o ID da store atual (importante para conteúdo multi-store)
+        $storeId = (int) $this->storeManager->getStore()->getId();
+
+        /**
+         * Cria uma instância de um bloco CMS dinamicamente
+         *
+         * @var \Magento\Cms\Block\Block $block
+         */
+        $block = $this->cmsBlockFactory->create();
+
+        // Define qual CMS Block será carregado (identifier no admin)
+        // Exemplo: Content > Blocks > Identifier = fulfillment_status
+        $block->setData('block_id', 'fulfillment_status');
+
+        // Define para qual store esse bloco deve ser renderizado
+        $block->setStoreId($storeId);
+
+        /**
+         * Retorna um array que será exposto no JavaScript do checkout
+         *
+         * No frontend você poderá acessar:
+         * window.checkoutConfig.avanti_customcheckout.fulfillment_status_html
+         *
+         * Esse valor conterá o HTML renderizado do CMS Block
+         */
+        return [
+            'avanti_customcheckout' => [
+                // Renderiza o bloco CMS em HTML e envia para o JS
+                'fulfillment_status_html' => $block->toHtml()
+            ]
+        ];
+    }
+}
