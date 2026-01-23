4af2de495e90c93b56edfe3033b33bd65bd92b20
Luiz Felipe <118226614+luizdevfelipe@users.noreply.github.com>
2026-01-23 18:01:43 -0300

    Comentários

--- src/app/code/Avanti/CustomCheckout/view/frontend/web/js/view/fulfillment-status.js
+++ src/app/code/Avanti/CustomCheckout/view/frontend/web/js/view/fulfillment-status.js
@@ -1,18 +1,25 @@
 define(['uiComponent'], function (Component) {
     'use strict';
 
+    // Extende o componente UI com as configurações padrão
     return Component.extend({
         defaults: {
+            // Define o template que será utilizado
             template: 'Avanti_CustomCheckout/fulfillment-status'
         },
 
+        // Função que retorna o HTML do status de cumprimento disponível na configuração do checkout
         getHtml: function () {
+            // Verifica se a configuração do checkout e o HTML do status de cumprimento estão disponíveis
             return (window.checkoutConfig &&
                 window.checkoutConfig.avanti_customcheckout &&
                 window.checkoutConfig.avanti_customcheckout.fulfillment_status_html)
                 ?
+                // Retorna o HTML se disponível
                 window.checkoutConfig.avanti_customcheckout.fulfillment_status_html
-                : '';
+                : 
+                // Retorna uma string vazia se não estiver disponível
+                '';
         }
     });
-}); 
+});
